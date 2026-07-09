cd4a9aa14f68231374c6732dc7236edc3fc6f721
DHARAVATH ANAND <ugs23108_aids.anand@cbit.org.in>
2026-07-09 14:07:24 +0530

    Time: 4 ms (60.22%) | Memory: 20.6 MB (23.51%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+
+        # Move all non-zero elements to the front
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
