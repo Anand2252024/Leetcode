61c0fc57281563c0b6f33256fc1fb7842cb8529f
DHARAVATH ANAND <ugs23108_aids.anand@cbit.org.in>
2026-08-04 23:22:38 +0530

    Time: 1444 ms (73.11%), Space: 30.2 MB (43.4%) - LeetHub

---
+++ 0327-count-of-range-sum/0327-count-of-range-sum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def countRangeSum(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: int
+        """
+        # Step 1: Build prefix sums
+        prefix = [0]
+        for num in nums:
+            prefix.append(prefix[-1] + num)
+
+        def sort_and_count(lo, hi):
+            if hi - lo <= 1:
+                return 0
+            mid = (lo + hi) // 2
+            count = sort_and_count(lo, mid) + sort_and_count(mid, hi)
+            j = k = mid
+            for left in prefix[lo:mid]:
+                while k < hi and prefix[k] - left < lower:
+                    k += 1
+                while j < hi and prefix[j] - left <= upper:
+                    j += 1
+                count += j - k
+            # merge step
+            prefix[lo:hi] = sorted(prefix[lo:hi])
+            return count
+
+        return sort_and_count(0, len(prefix))
