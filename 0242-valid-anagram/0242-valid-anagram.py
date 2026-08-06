ce1d61df0c79355a5fcd3dbf332eb215b878ac5d
DHARAVATH ANAND <ugs23108_aids.anand@cbit.org.in>
2026-08-06 20:28:59 +0530

    Time: 29 ms (8.07%), Space: 12.6 MB (67.51%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        
+        from collections import Counter
+        return Counter(s) == Counter(t)
