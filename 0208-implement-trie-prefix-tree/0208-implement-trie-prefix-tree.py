7c356a5522a44b17fe9efc8ca07695be48eda471
DHARAVATH ANAND <ugs23108_aids.anand@cbit.org.in>
2026-08-06 13:26:13 +0530

    Time: 89 ms (58.55%), Space: 39.1 MB (27.23%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,33 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}   # dictionary mapping char → TrieNode
+        self.is_end = False  # marks end of a word
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.is_end = True
+
+    def search(self, word):
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return node.is_end
+
+    def startsWith(self, prefix):
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return False
+            node = node.children[ch]
+        return True
