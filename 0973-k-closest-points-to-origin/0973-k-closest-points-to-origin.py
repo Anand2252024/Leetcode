7adb2a8cba6105e6c9ffa24a959ed13385e65ff6
DHARAVATH ANAND <ugs23108_aids.anand@cbit.org.in>
2026-08-11 19:06:02 +0530

    Time: 58 ms (67.17%), Space: 24.5 MB (77.86%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,17 +1,7 @@
+from typing import List
 import heapq
 
-class Solution(object):
-    def kClosest(self, points, k):
-        """
-        :type points: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
-        # max heap of size k
-        heap = []
-        for (x, y) in points:
-            dist = x*x + y*y
-            heapq.heappush(heap, (-dist, x, y))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        return [[x, y] for (_, x, y) in heap]
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # Use a min-heap based on squared distance
+        return heapq.nsmallest(k, points, key=lambda p: p[0]**2 + p[1]**2)
